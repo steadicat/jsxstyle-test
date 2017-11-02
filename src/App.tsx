c8779e956a5d705998a2263a39ceccd708ef339e
Stefano J. Attardi <stefano@attardi.org>
2017-11-02 10:44:55 -0700

    Highlight a few more issues

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,35 @@ import './App.css';
 import {Block, InlineBlock} from 'jsxstyle';
 
 const logo = require('./logo.svg');
+const width = '100%';
+const height = () => 150;
+
+// Without these lines, we get 'width/height is declared but never used'
+width;
+height;
+
+const Logo = (props: {width: number; height: number}) => (
+  <InlineBlock
+    component="img"
+    props={{src: logo, alt: 'logo'}}
+    animation="App-logo-spin infinite 20s linear"
+    height={80}
+    // {...props} // Causes '_Box is declared but never used'
+  />
+);
 
 class App extends React.Component {
   render() {
     return (
       <Block textAlign="center">
-        <Block backgroundColor="#222" height={150} padding={20} color="white">
-          <InlineBlock
-            component="img"
-            props={{src: logo, alt: 'logo'}}
-            animation="App-logo-spin infinite 20s linear"
-            height={80}
-          />
+        <Block
+          backgroundColor="#222"
+          width={width}
+          // backgroundImage={`url(${logo})`} // Causes '_Box is declared but never used'
+          // height={height()} // Causes '_Box is declared but never used'
+          padding={20}
+          color="white">
+          <Logo width={80} height={160} />
           <h2>Welcome to React</h2>
         </Block>
         <Block component="p" fontSize="large">
